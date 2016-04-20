2f2e5fde97c7cb10a712e67bd5f9fa8ac32332ea
Ravi Chebolu <arc095@motorola.com>
2016-04-20 15:12:54 -0500

    cpufreq_stats: Fix persist cpufreq all_time_in_state output
    Allocate memory for all_freq_table before cpufreq_stats_create_table()
    so that the freq_table is populated correctly.
    
    Change-Id: I9db031da863376bc5cc620d8e539a8b2fd4aedcd
    Signed-off-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-on: http://gerrit.mot.com/846819
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Matthew Senator <senator@motorola.com>
    Reviewed-by: Lian-Wei Wang <lian-wei.wang@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -1009,6 +1009,8 @@ static int __init cpufreq_stats_init(void)
 	create_all_freq_table();
 
 	get_online_cpus();
+	create_all_freq_table();
+
 	for_each_online_cpu(cpu)
 		cpufreq_stats_create_table(cpu);
 	put_online_cpus();
