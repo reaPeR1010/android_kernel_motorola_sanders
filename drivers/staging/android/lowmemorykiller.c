de13420e400972129cb473a0a2baa2a0bf7369e8
celtare21 <celtare21@gmail.com>
2019-01-24 10:46:46 +0000

    LMK: Disable debugging
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: reaPeR1010 <reaPeR10x10x@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -69,7 +69,7 @@ module_param_named(enable_lmk, enable_lmk, int,
 
 extern int extra_free_kbytes;
 
-static uint32_t lowmem_debug_level = 1;
+static uint32_t lowmem_debug_level = 0;
 static short lowmem_adj[6] = {
 	0,
 	1,
