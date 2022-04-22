52be1fddaca260eb8af694ec7502a532db319e34
reaPeR1010 <reaPeR10x10x@gmail.com>
2022-04-22 18:24:58 +0000

    build.sh: Shift to `AtomX Clang`
    Signed-off-by: reaPeR1010 <reaPeR10x10x@gmail.com>

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${TANGGAL}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=neutron
+COMPILER=atomx
 
 ##----------------------------------------------------------##
 # Specify Linker
